132a0be9074efdc44f656cb76525071ffba3d76c
Slogreco <shoshwright@hotmail.com>
2020-10-13 11:28:29 -0700

    added some questions

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Description of your project"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How can someone install their project on your computer?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Enter source for project screenshot/demo/code examples"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Enter link where questions about project can be directed"
+    },
 
 ];
 
